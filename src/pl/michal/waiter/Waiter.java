b8683a69bea17bd4149bb87279ed60583038087b
Michal Nowakowski <michal951now@gmail.com>
2018-11-08 18:55:18 +0100

    added waiter class to decide whether philospher is able to pickup knife and fork

---
+++ src/pl/michal/waiter/Waiter.java
@@ -0,0 +1,24 @@
+package pl.michal.waiter;
+
+import java.util.List;
+
+import pl.michal.fork.Fork;
+import pl.michal.knife.Knife;
+
+public class Waiter {
+	private int forkQuntity;
+	private int knifeQuantity;
+	private List<Fork> forks;
+	private List<Knife> knives;
+
+	public Waiter(int forkQuntit, int knifeQuantity) {
+		for (int i = 0; i < forkQuntity; i++) {
+			forks.add(new Fork(i + 1));
+		}
+
+		for (int i = 0; i < knifeQuantity; i++) {
+			knives.add(new Knife(i + 1));
+		}
+	}
+
+}
