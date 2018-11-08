742f523b01b728379e6c60841829adb8825094af
Michal Nowakowski <michal951now@gmail.com>
2018-11-08 18:11:56 +0100

    added 3 Lists to store philo.., forks and knives and initalized them

--- src/pl/michal/app/AppStart.java
+++ src/pl/michal/app/AppStart.java
@@ -1,9 +1,38 @@
 package pl.michal.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.michal.fork.Fork;
+import pl.michal.knife.Knife;
+import pl.michal.philosoper.Philosopher;
+
 public class AppStart {
 
+	final static int PHILOSOPHERS_QUANTITY = 5;
+	final static int KNIVES_QUANTITY = 5;
+	final static int FORKS_QUANTITY = 5;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		List<Philosopher> philosophers = new ArrayList<Philosopher>();
+		List<Fork> forks = new ArrayList<Fork>();
+		List<Knife> knives = new ArrayList<Knife>();
+
+		for (int i = 0; i < KNIVES_QUANTITY; i++) {
+			knives.add(new Knife(i+1));
+		}
+
+		for (int i = 0; i < FORKS_QUANTITY; i++) {
+			forks.add(new Fork(i+1));
+		}
+
+		for (int i = 0; i < PHILOSOPHERS_QUANTITY; i++) {
+			philosophers.add(new Philosopher(forks.get(i), knives.get(i)));
+
+			Thread thread = new Thread(philosophers.get(i), "Philosopher " + (i + 1));
+			thread.start();
+		}
 
 	}
 
